4aab9c274df7ac4db3550e33408eb2317f51f708
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-29 15:27:17 +0600

    category wiring

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meals/data/dummy_data.dart';
 import 'package:flutter_meals/resourse/widget/category_grid_item.dart';
+import 'package:flutter_meals/screen/meal_screen.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    // Navigator.of(context).push(route);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealScreen(
+          title: 'title',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +38,9 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
             ),
         ],
       ),
