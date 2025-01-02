11f6cbc80411ec83ffa648a1402fc2c44271c145
Fahim Muntasir <fahimmuntasir201@gmail.com>
2025-01-02 17:56:58 +0600

    implicit animation

--- lib/screen/meal_details_screen.dart
+++ lib/screen/meal_details_screen.dart
@@ -36,7 +36,25 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(
+                milliseconds: 300,
+              ),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  // turns: animation,
+                  turns: Tween<double>(
+                    begin: .5,
+                    end: 1,
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
