84bb68b510a6e7abd5f4db9ba71f301cf734c408
Fahim Muntasir <fahimmuntasir201@gmail.com>
2025-01-02 12:40:54 +0600

    Update meal_details_screen.dart

--- lib/screen/meal_details_screen.dart
+++ lib/screen/meal_details_screen.dart
@@ -13,6 +13,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var favoritesMeals = ref.watch(favoritesMealsProvider);
+
+    bool isFavorite = favoritesMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -32,7 +36,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
